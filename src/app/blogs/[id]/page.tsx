6c359da08ecf2d9d84b1c28243e19638d7019ef8
NamSnow <phamhoainam08102004@gmail.com>
2026-04-27 01:10:31 +0700

    31.

--- src/app/blogs/[id]/page.tsx
+++ src/app/blogs/[id]/page.tsx
@@ -2,23 +2,36 @@
 import Link from "next/link";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import useSWR, { Fetcher } from "swr";
 
 const ViewDetailBlog = ({ params }: { params: Promise<{ id: string }> }) => {
-  console.log("Check props", params.id);
+  const fetcher: Fetcher<IBlog, string> = (url: string) =>
+    fetch(url).then((res) => res.json());
+
+  const { data, error, isLoading } = useSWR(
+    `http://localhost:8000/blogs/${params.id}`,
+    fetcher,
+    {
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+    },
+  );
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
       <Link href={"/"}>Go back</Link>
 
       <Card className="text-center">
-        <Card.Header>Title</Card.Header>
+        <Card.Header>Title: {data?.title}</Card.Header>
         <Card.Body>
-          <Card.Text>
-            With supporting text below as a natural lead-in to additional
-            content.
-          </Card.Text>
+          <Card.Text>{data?.content}</Card.Text>
         </Card.Body>
-        <Card.Footer className="text-muted">Author</Card.Footer>
+        <Card.Footer className="text-muted">{data?.author}</Card.Footer>
       </Card>
     </div>
   );
